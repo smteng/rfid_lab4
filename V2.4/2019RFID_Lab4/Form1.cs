c8710f81376c84359024442aa0b7205e11ed97ec
smteng <tom1322kimo@gmail.com>
2021-11-27 13:43:16 +0800

    Implement sign/reset/read card
    without point processing

--- V2.4/2019RFID_Lab4/Form1.cs
+++ V2.4/2019RFID_Lab4/Form1.cs
@@ -11,7 +11,7 @@ namespace WindowsFormsApplication6
 {
     public partial class Form1 : Form
     {
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -81,6 +81,52 @@ namespace WindowsFormsApplication6
 
             }
         }
+        unsafe public string readData(string SNum, string BNum)
+        {
+            string retVal = "";
+            string KType = "60";  //參數預設
+
+            string ReadCmd = "020A15";  //讀取commend 0x02, 0x0A, 0x15,
+            string TX = ReadCmd + KType + LoadKey.Text + SNum + BNum;         //組成 Read Data Request
+
+            UInt32 uiLength, uiRead, uiResult, uiWritten;
+            byte[] ReadBuffer = new byte[0x40];
+            //byte[] WriteBuffer = new byte[] { 0x02, 0x0A, 0x15, 0x60, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x02, 0x01 };
+            byte[] WriteBuffer = StringToByteArray(TX);
+            byte[] sResponse = null;
+            sResponse = new byte[21];
+
+            EasyPOD.VID = 0xe6a;
+            EasyPOD.PID = 0x317;
+            Index = 1;
+            uiLength = 64;
+
+
+            fixed (MW_EasyPOD* pPOD = &EasyPOD)
+            {
+
+                dwResult = PODfuncs.ConnectPOD(pPOD, Index);
+
+                if ((dwResult != 0))
+                {
+                    MessageBox.Show("Not connected yet");
+                }
+                else
+                {
+                    EasyPOD.ReadTimeOut = 200;
+                    EasyPOD.WriteTimeOut = 200;
+
+                    dwResult = PODfuncs.WriteData(pPOD, WriteBuffer, 12, &uiWritten);    //Send a request command to reader  ,buffer 讀取12個 command 碼
+                    uiResult = PODfuncs.ReadData(pPOD, ReadBuffer, uiLength, &uiRead);  //Read the response data from reader
+
+                    retVal = BitConverter.ToString(ReadBuffer, 4, (Int32)uiRead - 4).Replace("-", "");  //HEX  從 4 之後開始 讀取 data (Response 欄位 0~4 為 02 12 15 00)
+                }
+                dwResult = PODfuncs.ClearPODBuffer(pPOD);
+                dwResult = PODfuncs.DisconnectPOD(pPOD);
+
+            }
+            return System.Text.Encoding.ASCII.GetString(StringToByteArray(retVal));
+        }
         /// <summary>
         /// 寫入
         /// </summary>
@@ -140,7 +186,80 @@ namespace WindowsFormsApplication6
 
             }
         }
+        unsafe public void writeData(string SNum, string BNum, string inputString)
+        {
+            string KType = "60";  //參數預設
 
+            string WriteCmd = "021A16";  //寫入commend 0x02, 0x1A, 0x16,
+
+            string str2 = inputString;
+            byte[] array = System.Text.Encoding.ASCII.GetBytes(str2);
+            string ASCIIstr2 = null;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int asciicode = (int)(array[i]);
+                ASCIIstr2 += Convert.ToString(asciicode.ToString("X"));
+            }
+            inputString = ASCIIstr2;
+
+            int initLen = 0;
+            try
+            {
+                initLen = inputString.Length;
+            } catch
+            {
+
+            }
+
+            System.Diagnostics.Debug.WriteLine("ASCII =" + inputString);
+            System.Diagnostics.Debug.WriteLine("1 len=" + inputString.Length);
+            if (inputString.Length < 32)
+            {
+                for (int i = 0; i < (32 - initLen); i++){
+                    inputString = inputString + "0";
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("2 len="+ inputString.Length);
+
+
+            string TX = WriteCmd + KType + LoadKey.Text + SNum + BNum + inputString;         //組成 Read Data Request
+
+            UInt32 uiLength, uiRead, uiResult, uiWritten;
+            byte[] ReadBuffer = new byte[0x40];
+            //byte[] WriteBuffer = new byte[] { 0x02, 0x0A, 0x15, 0x60, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x02, 0x01 };
+            byte[] WriteBuffer = StringToByteArray(TX);
+            byte[] sResponse = null;
+            sResponse = new byte[21];
+
+            EasyPOD.VID = 0xe6a;
+            EasyPOD.PID = 0x317;
+            Index = 1;
+            uiLength = 64;
+
+            fixed (MW_EasyPOD* pPOD = &EasyPOD)
+            {
+
+                dwResult = PODfuncs.ConnectPOD(pPOD, Index);
+
+                if ((dwResult != 0))
+                {
+                    MessageBox.Show("Not connected yet");
+                }
+                else
+                {
+                    EasyPOD.ReadTimeOut = 200;
+                    EasyPOD.WriteTimeOut = 200;
+
+                    dwResult = PODfuncs.WriteData(pPOD, WriteBuffer, 28, &uiWritten);    //Send a request command to reader  ,buffer 讀取12個 command 碼 + 16 byte data，所以讀28個
+                    uiResult = PODfuncs.ReadData(pPOD, ReadBuffer, uiLength, &uiRead);  //Read the response data from reader
+
+                    tebReadData.Text = BitConverter.ToString(ReadBuffer, 4, (Int32)uiRead - 4).Replace("-", "");  //HEX  從 4 之後開始 寫入 data (Response 欄位 0~4 為 02 12 16 00 ，這四個之後才是data)
+                }
+                dwResult = PODfuncs.ClearPODBuffer(pPOD);
+                dwResult = PODfuncs.DisconnectPOD(pPOD);
+
+            }
+        }
 
         /// <summary>
         /// StringToByteArray 字串轉型
@@ -197,7 +316,7 @@ namespace WindowsFormsApplication6
                 if (LoadKey.TextLength == 12)
                 {
                     BtnReadData.Enabled = true;
-                }  
+                }
             }
         }
 
@@ -221,7 +340,7 @@ namespace WindowsFormsApplication6
         }
 
         /// <summary>
-        /// 寫入防呆 
+        /// 寫入防呆
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -233,6 +352,75 @@ namespace WindowsFormsApplication6
                 BtnWriteData.Enabled = true;
             }
 
+        }
+
+        private void but_SignCard_Click(object sender, EventArgs e)
+        {
+            string id ;
+            string name ;
+            string signdate ;
+            string point = "";
+
+            id = textBox_MemID.Text;
+            name = textBox_MemName.Text;
+            signdate = textBox_SignDay.Text;
+            point = textBox_Point.Text;
+
+            System.Diagnostics.Debug.WriteLine(id);
+            writeData("01", "00", id);
+
+            System.Diagnostics.Debug.WriteLine(name);
+            writeData("01", "01", name);
+
+            System.Diagnostics.Debug.WriteLine(signdate);
+            writeData("01", "02", signdate);
+
+            System.Diagnostics.Debug.WriteLine(point);
+
+        }
+
+        private void textBox_MemID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tebReadData_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void but_ReadCard_Click(object sender, EventArgs e)
+        {
+            textBox_MemID2.Text = readData("01","00");
+            textBox_MemName2.Text = readData("01", "01");
+            textBox_SignDay2.Text = readData("01", "02");
+            textBox_Point2.Text = readData("01", "03");
+
+
+        }
+
+        private void textBox_MemID2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void but_CleanCard_Click(object sender, EventArgs e)
+        {
+            string id = "0";
+            string name = "0";
+            string signdate = "0";
+            string point = "0";
+
+            System.Diagnostics.Debug.WriteLine(id);
+            writeData("01", "00", id);
+
+            System.Diagnostics.Debug.WriteLine(name);
+            writeData("01", "01", name);
+
+            System.Diagnostics.Debug.WriteLine(signdate);
+            writeData("01", "02", signdate);
+
+            System.Diagnostics.Debug.WriteLine(point);
         }
 
         /// <summary>
